9e902a8aa3446877f07de84d0e5b89c2db90261f
CodeBySameeer <samirkathayat2080@gmail.com>
2026-04-30 11:42:37 +0545

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const http = require("http");
 const qrcode = require("qrcode");
 
 // ========== SETTINGS – CHANGE THESE ==========
-const API_KEY = "PASTE_YOUR_GROQ_API_KEY_HERE";   // from console.groq.com
+const API_KEY = "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";   // from console.groq.com
 
 const ALLOWED_CONTACTS = {
   "manikagurung": "She is my girlfriend  and we are in love since pat 3 4 years and we love each other You have to say 'Gooodmorning babee❤️' every 6 Am in nepali time so much and we can talk romantic and we will basically talk casually so ma uhh sanga jiskinxu pani ",
